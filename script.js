fcc9745af47cf7521ed68649ff1016b84efee1b8
Shapathaan <shapathaan@gmail.com>
2026-09-11 21:46:13 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const background =
+    document.querySelector(".background");
+
+
+// Desktop parallax animation
+if (
+    window.matchMedia("(pointer: fine)").matches
+) {
+
+    window.addEventListener(
+        "pointermove",
+        function (event) {
+
+            const x =
+                (event.clientX / window.innerWidth - 0.5) * 4;
+
+            const y =
+                (event.clientY / window.innerHeight - 0.5) * 3;
+
+            background.style.transform =
+                `scale(1.025)
+                 translate(${x}px, ${y}px)`;
+        }
+    );
+}
